cc5f06b81e12933b2cc7fd1baaa74bf5ee05d9d6
Jack Pietrok <jackpietrok@gmail.com>
2017-11-03 16:16:11 -0700

    piece classes created

--- Chess.py
+++ Chess.py
@@ -4,16 +4,24 @@ from Piece import *;
 
 # variable instantiation
 board = [];
-
+white_check = False;
+black_check = False;
+white_king_pos;
+black_king_pos;
 
 # Initialize Game Stats and Board
 def initialize():
+	white_check = False;
+	black_check = False;
+	white_king_pos = (0,4);
+	black_king_pos = (7,4);
+	black_king_pos;
 	board = [[ Rook("black"), Kight("black"), Bishop("black"), Queen("black"), King("black"), Bishop("black"), Kight("black"), Rook("black")],
 			 [ Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black")],
-			 [ null, null, null, null, null, null, null, null],
-			 [ null, null, null, null, null, null, null, null],
-			 [ null, null, null, null, null, null, null, null],
-			 [ null, null, null, null, null, null, null, null],
+			 [ None, None, None, None, None, None, None, None],
+			 [ None, None, None, None, None, None, None, None],
+			 [ None, None, None, None, None, None, None, None],
+			 [ None, None, None, None, None, None, None, None],
 			 [ Rook("white"), Kight("white"), Bishop("white"), Queen("white"), King("white"), Bishop("white"), Kight("white"), Rook("white")],
 			 [ Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white"), Pawn("white")]]
 	
@@ -24,13 +32,22 @@ def display_board():
 		for j in range(0,7):
 			print(board[i][j]);
 
+
 # Check for jepordy/checkmate
-def in_check():
+def analyze_check(self):
+	white_check = False;
+	black_check = False;
+	for x in range(0,7):
+		for y in range(0,7):
+			if(board[x][y] != None and board[x][y].team == "black" and white_king_pos in board[x][y].get_moves()):
+				white_check = True;
+			elif(board[x][y] != None and board[x][y].team == "white" and black_king_pos in board[x][y].get_moves()):
+				black_check = True;
 
 
 # Main Process
 def main():
-	while()
+	while();
 
 
 
--- Pieces.py
+++ Pieces.py
@@ -1,9 +1,9 @@
 import sys;
+from numpy import *;
 
 class Piece:
 	
-	def __init__(self,ty,te):
-		self.type = ty;
+	def __init__(self,te):
 		self.team = te;
 		
 	def switch_team(self):
@@ -15,50 +15,97 @@ class Piece:
 
 class Pawn(Piece):
 	
-	def get_moves(self,from_tuple,to_tuple):
+	def __init__(self,te):
+		Piece.__init__(self,te);
+		self.can_jump = True;
+	
+	def can_move(self,pos):
 		if(self.team == "white"):
-			return (to_tuple[0] - from_tuple[0] == 1) and (abs(to_tuple[1] - from_tuple[1]) == 1);
+			return [(pos[0] + 1 , pos[1] + 1) , (pos[0] - 1 , pos[1] + 1)];
 		else:
-			return (to_tuple[0] - from_tuple[0] == -1) and (abs(to_tuple[1] - from_tuple[1]) == 1);
+			return (to_tuple[0] != 0 and to_tuple[1] != 0) and (to_tuple[0] - from_tuple[0] == -1) and (abs(to_tuple[1] - from_tuple[1]) == 1);
 		
 	def __repr__(self):
 		return self.team + " pawn";
 
+
 class Rook(Piece):
 	
-	def get_moves(self,from_tuple,to_tuple):
+	def __init__(self,te):
+		Piece.__init__(self,te);
+		self.can_jump = False;
+	
+	def can_move(self,from_tuple,to_tuple):
 		return (from_tuple[0] == to_tuple[0] or from_tuple[1] == to_tuple[1]);
 	
+	def iterate(self,from_tuple,to_tuple):
+		# function assumes get_moves() has passed
+		direction = (to_tuple - from_tuple);
+		direction = (direction[0] / linalg.norm(direction)  ,  direction[1] / linalg.norm(direction));
+		return from_tuple + direction;
+
 	def __repr__(self):
 		return self.team + " rook";
 
+
 class Bishop(Piece):
 	
-	def get_moves(self,from_tuple,to_tuple):
+	def __init__(self,te):
+		Piece.__init__(self,te);
+		self.can_jump = False;
+	
+	def can_move(self,from_tuple,to_tuple):
 		return (from_tuple[0] - to_tuple[0] == from_tuple[1] - to_tuple[1]);
 	
+	def iterate(self,from_tuple,to_tuple):
+		# function assumes get_moves() has passed
+		direction = (to_tuple - from_tuple);
+		direction = (direction[0] / linalg.norm(direction)  ,  direction[1] / linalg.norm(direction));
+		return from_tuple + direction;
+
 	def __repr__(self):
 		return self.team + " bishop";
 	
+
 class Kight(Piece):
 	
-	def get_moves(self,from_tuple,to_tuple):
-		return ( abs(from_tuple[0] - to_tuple[0]) == 2  and  abs(from_tuple[1] - to_tuple[1]) == 1 )   or   ( abs(from_tuple[0] - to_tuple[0]) == 1  and  abs(from_tuple[1] - to_tuple[1]) == 2 );
+	def __init__(self,te):
+		Piece.__init__(self,te);
+		self.can_jump = True;
 	
+	def can_move(self,from_tuple,to_tuple):
+		return ( abs(from_tuple[0] - to_tuple[0]) == 2  and  abs(from_tuple[1] - to_tuple[1]) == 1 )   or   ( abs(from_tuple[0] - to_tuple[0]) == 1  and  abs(from_tuple[1] - to_tuple[1]) == 2 );
+
 	def __repr__(self):
 		return self.team + " knight";
 	
+
 class Queen(Piece):
 	
-	def get_moves(self,from_tuple,to_tuple):
+	def __init__(self,te):
+		Piece.__init__(self,te);
+		self.can_jump = False;
+	
+	def can_move(self,from_tuple,to_tuple):
 		return (from_tuple[0] - to_tuple[0] == from_tuple[1] - to_tuple[1]) or (from_tuple[0] == to_tuple[0] or from_tuple[1] == to_tuple[1]);
 	
+	def iterate(self,from_tuple,to_tuple):
+		# function assumes get_moves() has passed
+		direction = (to_tuple - from_tuple);
+		direction = (direction[0] / linalg.norm(direction)  ,  direction[1] / linalg.norm(direction));
+		return from_tuple + direction;
+
 	def __repr__(self):
 		return self.team + " queen";
 	
+
 class King(Piece):
 	
-	def get_moves(self,from_tuple,to_tuple):
+	def __init__(self,te):
+		Piece.__init__(self,te);
+		self.can_jump = True;
+	
+	def can_move(self,from_tuple,to_tuple):
 		return (abs(from_tuple[0] - to_tuple[0]) <= 1 and abs(from_tuple[1] - to_tuple[1]) <= 1) and (abs(from_tuple[0] - to_tuple[0]) != 0 or abs(from_tuple[1] - to_tuple[1]) != 0);
 	
 	def __repr__(self):

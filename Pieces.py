fe416e88580a450e7d29fa1fe53da983ffcb85be
Jack Pietrok <jackpietrok@gmail.com>
2017-11-07 10:12:26 -0800

    minor edits
    re-evaluating life choices (also project structuring)

--- Pieces.py
+++ Pieces.py
@@ -13,30 +13,38 @@ class Piece:
 			team = "white";
 		
 
+# Pawn piece
 class Pawn(Piece):
 	
 	def __init__(self,te):
 		Piece.__init__(self,te);
 		self.can_jump = True;
 	
-	def can_move(self,pos):
+	def get_moves(self,pos):
 		if(self.team == "white"):
-			return [(pos[0] + 1 , pos[1] + 1) , (pos[0] - 1 , pos[1] + 1)];
+			return [(pos[0] + 1 , pos[1] + 1) , (pos[0] + 1 , pos[1] - 1)];
 		else:
-			return (to_tuple[0] != 0 and to_tuple[1] != 0) and (to_tuple[0] - from_tuple[0] == -1) and (abs(to_tuple[1] - from_tuple[1]) == 1);
+			return [(pos[0] - 1 , pos[1] + 1) , (pos[0] - 1 , pos[1] - 1)];
 		
 	def __repr__(self):
 		return self.team + " pawn";
 
 
+# Rook piece (castle)
 class Rook(Piece):
 	
 	def __init__(self,te):
 		Piece.__init__(self,te);
 		self.can_jump = False;
 	
-	def can_move(self,from_tuple,to_tuple):
-		return (from_tuple[0] == to_tuple[0] or from_tuple[1] == to_tuple[1]);
+	def get_moves(self,pos):
+		arr = [];
+		for y in range(0,7):
+			if(y != pos[1]):	
+				arr += (pos[0] , y);
+			if(y != pos[0]):
+				arr += (y , pos[1]);
+		return arr;
 	
 	def iterate(self,from_tuple,to_tuple):
 		# function assumes get_moves() has passed
@@ -48,14 +56,20 @@ class Rook(Piece):
 		return self.team + " rook";
 
 
+# Bishop piece (priest)
 class Bishop(Piece):
 	
 	def __init__(self,te):
 		Piece.__init__(self,te);
 		self.can_jump = False;
 	
-	def can_move(self,from_tuple,to_tuple):
-		return (from_tuple[0] - to_tuple[0] == from_tuple[1] - to_tuple[1]);
+	def get_moves(self,pos):
+		arr = [];
+		count = 1;
+		for i in range(pos[1],7):
+			arr += (pos[0]+count,pos[1]+count);
+			arr += (pos[0]-count,pos[1]-count);
+			count += 1;
 	
 	def iterate(self,from_tuple,to_tuple):
 		# function assumes get_moves() has passed
@@ -67,26 +81,28 @@ class Bishop(Piece):
 		return self.team + " bishop";
 	
 
+# Knight piece (horse)
 class Kight(Piece):
 	
 	def __init__(self,te):
 		Piece.__init__(self,te);
 		self.can_jump = True;
 	
-	def can_move(self,from_tuple,to_tuple):
+	def get_moves(self,from_tuple,to_tuple):
 		return ( abs(from_tuple[0] - to_tuple[0]) == 2  and  abs(from_tuple[1] - to_tuple[1]) == 1 )   or   ( abs(from_tuple[0] - to_tuple[0]) == 1  and  abs(from_tuple[1] - to_tuple[1]) == 2 );
 
 	def __repr__(self):
 		return self.team + " knight";
 	
 
+# Queen piece
 class Queen(Piece):
 	
 	def __init__(self,te):
 		Piece.__init__(self,te);
 		self.can_jump = False;
 	
-	def can_move(self,from_tuple,to_tuple):
+	def get_moves(self,from_tuple,to_tuple):
 		return (from_tuple[0] - to_tuple[0] == from_tuple[1] - to_tuple[1]) or (from_tuple[0] == to_tuple[0] or from_tuple[1] == to_tuple[1]);
 	
 	def iterate(self,from_tuple,to_tuple):
@@ -99,13 +115,14 @@ class Queen(Piece):
 		return self.team + " queen";
 	
 
+# King piece
 class King(Piece):
 	
 	def __init__(self,te):
 		Piece.__init__(self,te);
 		self.can_jump = True;
 	
-	def can_move(self,from_tuple,to_tuple):
+	def get_moves(self,from_tuple,to_tuple):
 		return (abs(from_tuple[0] - to_tuple[0]) <= 1 and abs(from_tuple[1] - to_tuple[1]) <= 1) and (abs(from_tuple[0] - to_tuple[0]) != 0 or abs(from_tuple[1] - to_tuple[1]) != 0);
 	
 	def __repr__(self):
